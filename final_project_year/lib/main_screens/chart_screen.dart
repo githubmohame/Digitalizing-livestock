d5c337e368b92a1cc45e60134bc10046db900db2
githubmohame <mohammmedayman5@gmail.com>
2022-11-19 23:51:47 +0200

    test charts

--- final_project_year/lib/main_screens/chart_screen.dart
+++ final_project_year/lib/main_screens/chart_screen.dart
@@ -1,38 +1,94 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ChartScreen extends StatelessWidget {
-  const ChartScreen({Key? key}) : super(key: key);
-
+  ChartScreen({Key? key}) : super(key: key);
+  TooltipBehavior tooltipBehavior = TooltipBehavior(enable: true);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        SfCartesianChart(
-          series: <ChartSeries>[
-            BarSeries<Test,String>(
-                dataSource: getTest(),
-                xValueMapper: (Test datum, index) =>datum.gavernorate ,
-                yValueMapper: (Test datum, index) =>datum.weight)
-          ],
-        )
-      ],
-    ));
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+        ),
+        backgroundColor: Color(0x0FFFb0b348),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: SfCircularChart(
+                  title: ChartTitle(text: 'tell me'),
+                  legend: Legend(isVisible: true),
+                  tooltipBehavior: tooltipBehavior,
+                  series: <CircularSeries>[
+                    PieSeries<Test, String>(
+                        enableTooltip: true,
+                        groupTo: 2,
+                        //groupMode: CircularChartGroupMode.point,
+                        dataLabelSettings: DataLabelSettings(isVisible: true),
+                        dataSource: getTest(),
+                        xValueMapper: (Test datum, index) => datum.gavernorate,
+                        yValueMapper: (Test datum, index) => datum.weight)
+                  ],
+                ),
+              ),
+              Container(height: 1300 ,
+                child: SfCartesianChart(
+                  title: ChartTitle(text: 'tell me'),
+                  series: <ChartSeries>[
+                    BarSeries<Test, String>(  
+                        dataSource: getTest(),
+                        xValueMapper: (Test datum, index) => datum.gavernorate,
+                        yValueMapper: (Test datum, index) => datum.weight)
+                  ],
+                  primaryXAxis: CategoryAxis(),
+                ),
+              ),
+               SfCartesianChart(
+                title: ChartTitle(text: 'tell me'),
+                series: <ColumnSeries<Test,String>>[
+                  ColumnSeries<Test, String>(
+                      dataSource: getTest(),
+                      xValueMapper: (Test datum, index) => datum.gavernorate,
+                      yValueMapper: (Test datum, index) => datum.weight)
+                ],
+                primaryXAxis: CategoryAxis(),
+              ),
+                SfCartesianChart(
+                title: ChartTitle(text: 'tell me'),
+                series: <ChartSeries>[
+                  BarSeries<Test, String>(
+                      dataSource: getTest(),
+                      xValueMapper: (Test datum, index) => datum.gavernorate,
+                      yValueMapper: (Test datum, index) => datum.weight)
+                ],
+                primaryXAxis: CategoryAxis(),
+              ),
+                SfCartesianChart(
+                title: ChartTitle(text: 'tell me'),
+                series: <ChartSeries>[
+                  BarSeries<Test, String>(
+                      dataSource: getTest(),
+                      xValueMapper: (Test datum, index) => datum.gavernorate,
+                      yValueMapper: (Test datum, index) => datum.weight)
+                ],
+                primaryXAxis: CategoryAxis(),
+              ),
+               
+            ],
+          ),
+        ));
   }
 }
 
 List<Test> getTest() {
-  return [
-    Test(gavernorate: 'jjkkuu',weight: 6789768667),
-     Test(gavernorate: 'jjkkuu',weight: 6789768667),
-      Test(gavernorate: 'jjkkuu',weight: 6789768667),
-  ];
+  return List<Test>.generate(100, (index) => Test(gavernorate: "t"+index.toString(),weight: (2*index).toDouble())).reversed.toList();
 }
 
 class Test {
-  String gavernorate;
-  int weight;
+  final String gavernorate;
+  final double weight;
   Test({
     required this.gavernorate,
     required this.weight,
